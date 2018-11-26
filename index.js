d3e07b2f01464477a1fbea89568b6791e7cd8e85
yeonsukchoi <yeosukchoi@github.com>
2018-11-26 11:26:19 +0900

    modify index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('You did well!');
 });
+
+app.run(
+  host=os.getenv('LISTEN', '0.0.0.0'),
+  port=int(os.getenv('PORT','3000'))
+  )
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
