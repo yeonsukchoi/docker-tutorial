e4903e59341d73927ec564acbac6879c102eb864
EC2 Default User <ec2-user@ip-172-31-30-24.ap-northeast-2.compute.internal>
2018-11-27 10:07:48 +0000

    remove test code

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ app.get('/', function (req, res) {
   res.send('You did well!');
 });
 
-app.run(
-  host=os.getenv('LISTEN', '0.0.0.0'),
-  port=int(os.getenv('PORT','3000'))
-  )
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
